5434b6f1fe82683fb182c5bde22dbd964632e3f2
bplx <satosummering@protonmail.com>
2022-05-04 21:09:29 -0400

    woo fix

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
+// includes
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+// includes for system-specific functions
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+char* getinput() {
+	char buf[101];
+	fgets(buf, 101, stdin);
+	char* dyn = malloc(strlen(buf) * sizeof(char)); 
+	strcpy(dyn, buf);
+	return dyn;
+}
 
 int main() {
-	std::cout << "yest" << std:endl;
+	char* input = getinput();
+	fputs(input, stdout);
+	free(input);
+	return 0;
 }
